f6cb962c9bfa627d0b7739e486fbd14b0f391f21
Stefan Voicu <stefanvoicu@Stefans-MacBook-Air.local>
2024-03-15 20:19:59 -0400

    did it work

---
+++ chat petals/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './style.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+     <div>
+           <header>
+               {/* <link rel="stylesheet" href="style.css"> */}
+           </header>
+           <body>
+               <section>
+                   <header>
+                     <h1>Chatbox</h1>
+                   </header>
+                   <main>                  
+                   </main>
+                   <form>
+                       {/* <input type="text" placeholder="Message..."> */}
+                       <button type="submit"></button>
+                   </form>
+               </section>
+               <script type="text/javascript" src="index.js"></script>
+           </body>
+     </div>  
+    );
+  }
+  
+
+export default App;
